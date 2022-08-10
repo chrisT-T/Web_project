d08ca1b72f95209518f9dc75fef0298fd930394d
adfwer233 <ba123698745@126.com>
2022-08-10 22:21:34 +0800

    fix merge

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-const app = createApp(App)
-  .use(VueAxios, axios)
-
-app.directive('focus', (el, binding) => {
-  el.querySelector('input').focus()
-})
-
-app
-  .use(store)
-  .use(router)
-  .use(ElementPlus, { size: 'small', zIndex: 3000 })
-  .mount('#app')
-=======
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
